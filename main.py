9740d10f904fc135fa5854add9d421127427f86d
Levente Botos <levinandi98@gmail.com>
2023-03-17 07:20:29 +0000

    Update 3 files

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import improv as impr
+import pit
 
-x = impr.inp("1", "float")
-y = impr.inp("2", "float")  # 32
-z = impr.inp("3", "float")
 
-print(impr.inp("4", "bool"))
-
-if x < y + z and y < z + x and z < x + y:
-    print("har y")
-else:
-    print("har n")
-
-if z**2 == x**2 + y**2 or x**2 == z**2 + y**2 or y**2 == x**2 + z**2:
-    print("pit y")
-else:
-    print("pit n")
+pit.check()
---
+++ pit.py
@@ -0,0 +1,17 @@
+import improv as impr
+
+
+def check():
+    x = impr.inp("1", "float")
+    y = impr.inp("2", "float")  # 32
+    z = impr.inp("3", "float")
+
+    if x < y + z and y < z + x and z < x + y:
+        print("har y")
+    else:
+        print("har n")
+
+    if z**2 == x**2 + y**2 or x**2 == z**2 + y**2 or y**2 == x**2 + z**2:
+        print("pit y")
+    else:
+        print("pit n")
