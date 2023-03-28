73958687cd3e656b251c8e621a45480b00d00522
Levente Botos <levinandi98@gmail.com>
2023-03-28 10:26:43 +0000

    Update heron.cpython-311.pyc and heron.py

--- heron.py
+++ heron.py
@@ -1,7 +1,15 @@
 import improv
+import math
 
-n1 = improv.inp("Szám 1: ", "int")
-n2 = improv.inp("Szám 2: ", "int")
-n3 = improv.inp("Szám 3: ", "int")
+a = improv.inp("Szám 1: ", "float")
+b = improv.inp("Szám 2: ", "float")
+c = improv.inp("Szám 3: ", "float")
 
-print(f'{n1}, {n2}, {n3}')
+print(f'Oldalak: {a}, {b}, {c}')
+
+if a > 0 and b > 0 and c > 0 and a < b + c and b < a + c and c < a+b:
+    s = (a + b + c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print(f"A háromszög területe: {area}.")
+else:
+    print("nem 3szog.")
