a2b287858ae19cab8a8d2d2f860f415bb21cbce9
Levente Botos <levinandi98@gmail.com>
2023-03-28 10:46:25 +0000

    Update 3 files

--- heron.py
+++ heron.py
@@ -11,6 +11,5 @@ if a > 0 and b > 0 and c > 0 and a < b + c and b < a + c and c < a+b:
     s = (a + b + c)/2
     area = math.sqrt(s*(s-a)*(s-b)*(s-c))
     print(f"A háromszög területe: {area}.")
-    print(type(a))
 else:
     print("nem 3szog.")
