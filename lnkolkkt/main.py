f48d44c80f1560af64e0e7dbf3b5eae3df20c387
Levente Botos <botos.levente2007@gmail.com>
2024-09-12 11:35:53 +0200

    2.nd ora

--- lnkolkkt/main.py
+++ lnkolkkt/main.py
@@ -1,20 +1,30 @@
 number1i = input("első szám: ")
 number2i = input("második szám: ")
 
-if number1i.isnumeric() and number2i.isnumeric():
-    numbaer1 = int(number1i)
-    number2 = int(number2i)
-    
-    while number1 != 0 or number2 != 0:
+
+def calcLnko(number1, number2):
+ while number1 != 0 or number2 != 0:
         if (number1 > number2):
             number1 -= number2
         elif (number1 < number2):
             number2 -= number1
         elif (number1 == number2):
-            print(f'A lenagyobb közös osztója {number1i} és {number2i}-nek: {number1}')
-            number1 = 0
-            number2 = 0
+            return number1
         else:
             print("Hiba a számokkal")
+
+def calckLkkt(number1, number2, lnko):
+    return int(int(number1 * number2) / lnko)
+
+if number1i.isnumeric() and number2i.isnumeric():
+    number1 = int(number1i)
+    number2 = int(number2i)
+    
+    lnko = calcLnko(number1, number2)
+    print(f'A lenagyobb közös osztója {number1i} és {number2i}-nek: {lnko}')
+
+    lkkt = calckLkkt(number1, number2, lnko)
+    print(f'A legkissebb közös többszöröse {number1i} és {number2i}-nek: {lkkt}')
+   
 else: 
     print("Nem szám volt malamelyik bemenet!")
