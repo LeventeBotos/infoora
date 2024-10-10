6c1fcfef3d96aff6b5b919559f1b06cde68ba7d9
Levente Botos <botos.levente2007@gmail.com>
2024-10-10 22:13:19 +0200

    s

---
+++ szamparok/index.py
@@ -0,0 +1,31 @@
+
+def calcLnko(number1, number2):
+ while number1 != 0 :
+        while number2 != 0 :
+            if (number1 > number2):
+                number1 -= number2
+            elif (number1 < number2):
+                number2 -= number1
+            elif (number1 == number2):
+             return number1
+            else:
+                print("Hiba a számokkal")
+        return number2
+ return number1
+        
+
+def isRelPrime(i, j):
+    lnko = calcLnko(i, j)
+    if lnko == 1:
+        return True
+    else: 
+        return False
+
+list = []
+for i in range(100):
+    for j in range(i, 100):
+        if isRelPrime(i, j):
+            print(f"{i} és {j} relativ primek")
+            list.append(f"{i} és {j}")
+
+print(len(list))
