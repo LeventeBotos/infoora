124047b273f233447da0470adc16934b3e197949
Levente Botos <botos.levente2007@gmail.com>
2024-10-17 20:27:04 +0200

    fuck me

---
+++ leastrows/main.py
@@ -0,0 +1,5 @@
+a, b = 1/2, -3
+# print( int(-b * 2 - a - 1), int(-2 - b**2 - a - 1), int(-(2 * b)**2 - a - 1), int((-2*b)**2 - a - 1), int(-2*b - a**2 - 1), int(-2*b - (a-1)**2))
+
+txt = "Hello {} {}"
+print(txt.format("Botos", "Levente"))
